3160e486d9c54b666ee6a60d05cb7ec003446b09
amoxley <alex.moxley@gmail.com>
2022-03-17 17:58:00 -0700

    start backend auth: creates users and session controllers

---
+++ frontend/sidcord.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const h1 = <h1>Sidccord</h1>;
+  const root = document.querySelector('#root');
+
+  ReactDOM.render(h1, root);
+})
