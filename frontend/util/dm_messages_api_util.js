4d721a349a23e09af3775aa3f043d85b73411dfd
amoxley <alex.moxley@gmail.com>
2022-04-14 15:16:36 -0700

    DM APIUtils

---
+++ frontend/util/dm_messages_api_util.js
@@ -0,0 +1,29 @@
+export const fetchDmMessage = dmMessageId => {
+  return $.ajax({
+    method: 'GET',
+    url: `api/dm_messages/${dmMessageId}`
+  })
+};
+
+export const createDmMessage = (dmServerId, dmMessage) => {
+  return $.ajax({
+    method: 'POST',
+    url: `api/dm_servers/${dmServerId}/dm_messages`,
+    data: { dmMessage: dmMessage}
+  })
+};
+
+export const updateDmMessage = dmMessage => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `api/dm_messages/${dmMessage.id}`,
+    data: { dmMessage: dmMessage }
+  })
+};
+
+export const deleteDmMessage = dmMessageId => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `api/dm_messages/${dmMessageId}`
+  })
+}
