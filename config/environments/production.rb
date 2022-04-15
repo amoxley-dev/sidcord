8b7afdc736ec16f0a7e4c7224602cf605c8ae90f
amoxley <alex.moxley@gmail.com>
2022-04-14 21:40:51 -0700

    heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
+
   config.web_socket_server_url = "wss://sidcord-1.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://sidcord-1.herokuapp.com', 'http://sidcord-1.herokuapp.com'] 
 end
