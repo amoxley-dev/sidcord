e4a7b66097d579784ae5a97ca53fd5e6924ee407
amoxley <alex.moxley@gmail.com>
2022-04-18 12:23:12 -0700

    adds to channel creation

--- frontend/components/channels/create_channel.jsx
+++ frontend/components/channels/create_channel.jsx
@@ -23,8 +23,27 @@ function CreateChannel(props) {
 
   return (
     <div className="create-channel-container">
+      <svg 
+          className="close-button"
+          width="24"
+          height="24" 
+          viewBox="0 0 24 24"
+          onClick={() => props.closeModal()}
+        ><path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"></path>
+      </svg>
+      <header className="create-channel-header">
+        <h2>Create Text Channel</h2>
+        <div>in Text Channels</div>
+      </header>
       <form className="channel-create-form" onSubmit={e => handleSubmit(e)}>
+        <label>
+          CHANNEL NAME
+        </label>
         <input type="text" value={name} onChange={(e) => setName(e.currentTarget.value)}/>
+        <div>
+          <div onClick={() => props.closeModal()}>Cancel</div>
+          <button type="submit">Create Channel</button>
+        </div>
       </form>
     </div>
   )
